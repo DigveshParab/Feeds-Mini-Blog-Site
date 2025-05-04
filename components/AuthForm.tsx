d3bce463db9ce139017551929960931831c84c1f
Digvesh parab <159231475+DigveshParab@users.noreply.github.com>
2025-05-04 18:16:36 +0530

    style fixes for login button

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -80,7 +80,7 @@ export default function AuthForm() {
         )}
         <input type="email" placeholder="Email" className="w-full p-2 border rounded" value={email} onChange={e=>setEmail(e.target.value)} required/>
         <input type="password" placeholder="Password" className="w-full p-2 border rounded" value={password} onChange={e=>setPassword(e.target.value)} required/>
-        <button type="submit" className="w-full bg-black text-white py-2 rounded hover:bg-gray-900 disabled:bg-gray-400"  disabled={!isFilled}>
+        <button type="submit" className="w-full flex justify-center bg-black text-white py-2 rounded hover:bg-gray-900 disabled:bg-gray-400"  disabled={!isFilled}>
           {isLogin ? !is_laoding ? "Login" : <div className="ml-2 h-4 w-4 border-2 border-white border-t-transparent rounded-full animate-spin" /> : !is_laoding ? "Sign Up":<div className="ml-2 h-4 w-4 border-2 border-white border-t-transparent rounded-full animate-spin" />}
         </button>
       </form>
