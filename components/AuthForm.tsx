3c5b6f4dd52af6dffb74e58a1cafffb725b982ee
Digvesh parab <159231475+DigveshParab@users.noreply.github.com>
2025-05-04 18:14:01 +0530

    fixes in auth

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -81,7 +81,7 @@ export default function AuthForm() {
         <input type="email" placeholder="Email" className="w-full p-2 border rounded" value={email} onChange={e=>setEmail(e.target.value)} required/>
         <input type="password" placeholder="Password" className="w-full p-2 border rounded" value={password} onChange={e=>setPassword(e.target.value)} required/>
         <button type="submit" className="w-full bg-black text-white py-2 rounded hover:bg-gray-900 disabled:bg-gray-400"  disabled={!isFilled}>
-          {isLogin ? is_laoding ? "Login" : <div className="ml-2 h-4 w-4 border-2 border-white border-t-transparent rounded-full animate-spin" /> : is_laoding ? "Sign Up":<div className="ml-2 h-4 w-4 border-2 border-white border-t-transparent rounded-full animate-spin" />}
+          {isLogin ? !is_laoding ? "Login" : <div className="ml-2 h-4 w-4 border-2 border-white border-t-transparent rounded-full animate-spin" /> : !is_laoding ? "Sign Up":<div className="ml-2 h-4 w-4 border-2 border-white border-t-transparent rounded-full animate-spin" />}
         </button>
       </form>
       <p className="text-center text-sm text-gray-600 mt-4">
