7439886d1119e27e7ed9a8cb7a97007d3d6d7500
Digvesh parab <159231475+DigveshParab@users.noreply.github.com>
2025-05-04 21:58:38 +0530

    cleanup

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,9 @@
 export {middleware} from "./middleware/auth"
-//todo: find out about this how this works
 export const config = {
     matcher: [
       '/api/auth/me',
       '/api/post/create',
-      '/api/post/edit/:path*', // <- Matches /api/post/edit/[id] and any subpaths
+      '/api/post/edit/:path*', 
       '/api/post/delete/:path*',
       '/api/post/:path*',
       '/api/post/global',
